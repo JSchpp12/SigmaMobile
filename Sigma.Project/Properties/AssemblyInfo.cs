57c03063ecdaeb9fc23eb9af74d44f5d00a3fe7f
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2020-09-08 21:39:35 -0400

    Changed Application Name

--- Sigma.Project/Properties/AssemblyInfo.cs
+++ Sigma.Project/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sigma.Android")]
+[assembly: AssemblyTitle("Sigma.Project")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sigma.Android")]
+[assembly: AssemblyProduct("Sigma.Project")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
