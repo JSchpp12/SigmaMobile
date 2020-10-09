d8416f703ec9a5407e6120016a383e1c679f4556
Jacob Schaupp <jschaup2@rockets.utoledo.edu>
2020-10-09 18:40:29 -0400

    Initial Message Class

---
+++ Sigma.Client/Classes/Message.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sigma.Networking.Classes
+{
+    class Message
+    {
+        struct msgType
+        {
+            public bool isTime;
+            public bool isValid;
+            public void IsTime() { this.isTime = true; this.isValid = true;  }
+            public void Invalid() { this.isTime = false; this.isValid = false; }
+        }
+        private msgType type; 
+        private string message; 
+
+        Message(string inMessage)
+        {
+            this.message = inMessage; 
+        }
+
+        public bool IsValid() { return this.type.isValid;  }
+        private void processMessage() 
+        {
+            if (this.message.Contains("TIME:"))
+            {
+                this.type.IsTime();
+            }
+            else
+            {
+                this.type.Invalid(); 
+            }
+        }
+    }
+}
